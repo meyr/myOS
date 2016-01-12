c6bd475680dc4bf83e89da5de876b31d48f2eca8
albert <wenhung.yang@auo.com>
2016-01-12 17:08:50 +0800

    add LED(pa5) SysTick function
    1. add control GPIO PA5 to light up LED2
    2. add SysTick to get system tick

--- main.c
+++ main.c
@@ -1,83 +1,77 @@
 #include "reg.h"
 
+__IO uint32_t uwTick;
+
 void SystemInit(void)
 {
-  /* Reset the RCC clock configuration to the default reset state(for debug purpose) */
-  /* Set HSION bit */
-  RCC->CR |= (uint32_t)0x00000001;
-
-  /* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
-  RCC->CFGR &= (uint32_t)0xF8FF0000;
-  
-  /* Reset HSEON, CSSON and PLLON bits */
-  RCC->CR &= (uint32_t)0xFEF6FFFF;
-
-  /* Reset HSEBYP bit */
-  RCC->CR &= (uint32_t)0xFFFBFFFF;
-
-  /* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
-  RCC->CFGR &= (uint32_t)0xFF80FFFF;
-
-  /* Disable all interrupts and clear pending bits  */
-  //RCC->CIR = 0x009F0000;
-  RCC->CIR = 0x00000000;
+	/* Reset the RCC clock configuration to the default reset state(for debug purpose) */
+	/* Set HSION bit */
+	RCC->CR |= (uint32_t)0x00000001;
+	
+	/* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
+	RCC->CFGR &= (uint32_t)0xF8FF0000;
+	
+	/* Reset HSEON, CSSON and PLLON bits */
+	RCC->CR &= (uint32_t)0xFEF6FFFF;
+	
+	/* Reset HSEBYP bit */
+	RCC->CR &= (uint32_t)0xFFFBFFFF;
+	
+	/* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
+	RCC->CFGR &= (uint32_t)0xFF80FFFF;
+	
+	/* Disable all interrupts and clear pending bits  */
+	//RCC->CIR = 0x009F0000;
+	RCC->CIR = 0x00000000;
     
-  /* config HSI */
-  	/* enable HSI */
-	RCC->CR |= 0x00000001;
-	/* wait HSI RDY */
-	while ((RCC->CR & 0x00000002) != 0x00000002);
+	/* config HSI */
+		/* enable HSI */
+		RCC->CR |= 0x00000001;
+		/* wait HSI RDY */
+		while ((RCC->CR & 0x00000002) != 0x00000002);
 
-  /* config PLL */
-  	/* set PLL source to HSI/2 */
-  	RCC->CFGR &= 0xFFFEFFFF;
+	/* config PLL */
+		/* set PLL source to HSI/2 */
+		RCC->CFGR &= 0xFFFEFFFF;
+		
+		/* set PLLMUL to x16 */
+		RCC->CFGR |= 0x00380000;
+		
+		/* set PLL ON */
+		RCC->CR |= 0x01000000;
+		
+		/* wait PLL RDY */
+		while ((RCC->CR & 0x02000000) != 0x02000000);
 
-  	/* set PLLMUL to x16 */
-  	RCC->CFGR |= 0x00380000;
+	/* set APB1 prescaler to 2 */
+	RCC->CFGR |= 0x00000400;
+	
+	/* enable prefetch */
+	FLASH->ACR |= 0x00000010;
+	
+	/* set wait 2 cyccle for 64Mhz */
+	FLASH->ACR &= 0xfffffff8;
+	FLASH->ACR |= 0x00000002;
 
-	/* set PLL ON */
-	RCC->CR |= 0x01000000;
-
-	/* wait PLL RDY */
-	while ((RCC->CR & 0x02000000) != 0x02000000);
-
-  /* set APB1 prescaler to 2 */
-  RCC->CFGR |= 0x00000400;
-
-  /* enable prefetch */
-  FLASH->ACR |= 0x00000010;
-
-  /* set wait 2 cyccle for 64Mhz */
-  FLASH->ACR &= 0xfffffff8;
-  FLASH->ACR |= 0x00000002;
-
-  /* set SYSCLK to PLLCLK */
+	/* set SYSCLK to PLLCLK */
 	RCC->CFGR &= 0xFFFFFFFC;
 	RCC->CFGR |= 0x00000002;
 
-  /* set MCO to SYSCLK */
+ 	/*  stm32f103rb nucleo board PD0/MCO pin connect to
+	 *  stm32f103cbt6 jtag ic mco output the freq. is the same
+	 *  to this IC input external oscillator freq.
+	 */
+	
+ 	/* enable GPIOD */
+ 	//RCC->APB2ENR |= 0x00000020;
+ 	/* enable AFIO */
+ 	//RCC->APB2ENR |= 0x00000001;
+ 	/* remap pd0/pd1 to OSC_IN/OSC_OUT */
+ 	//AFIO->MAPR |= 0x00008000; 
+ 	/* set MCO to SYSCLK */
  	//RCC->CFGR &= 0xF8FFFFFF;
  	//RCC->CFGR |= 0x04000000;
 
-  /* enable UART1 */
-  RCC->APB2ENR |= 0x00004000;
-  
-  /* set UART1 parameter */
-  USART1->SR   = 0x000000c0;	
-  USART1->DR   = 0x00000000;
-  USART1->BRR  = 0x0000022c;	/* 0x22 = 34, 0xc = 12 => 34 + 12/16 = 34.75, baud = 64/(16*34.75) = 1151079 hz */
-  USART1->CR1  = 0x0000100c;	/* 8 data bit , parity none, even parity*/ 
-  USART1->CR2  = 0x00000000;
-  USART1->CR3  = 0x00000000;
-  USART1->GTPR = 0x00000000; 
-  /* enable UART1 */
-  USART1->CR1 |= 0x00002000;
-
-  /* enable GPIOA */
-  RCC->APB2ENR |= 0x00000004;
-  /* switch pa9/pa10 to uart1 */
-  GPIOA->CRH &= 0xfffff00f;
-  GPIOA->CRH |= 0x000008b0;
 }
 
 void print_str(const char *str)
@@ -88,10 +82,92 @@ void print_str(const char *str)
                 str++;
         }
 }
-						
+
+void initUART(void)
+{
+
+	/* UART1 configure */
+	/* enable UART1 clock */
+	RCC->APB2ENR |= 0x00004000;
+		
+	/* set UART1 parameter */
+	USART1->SR   = 0x000000c0;	
+	USART1->DR   = 0x00000000;
+	USART1->BRR  = 0x0000022c;	/* 0x22 = 34, 0xc = 12 => 34 + 12/16 = 34.75, baud = 64/(16*34.75) = 1151079 hz */
+	USART1->CR1  = 0x0000100c;	/* 8 data bit , parity none, even parity*/ 
+	USART1->CR2  = 0x00000000;
+	USART1->CR3  = 0x00000000;
+	USART1->GTPR = 0x00000000; 
+	/* enable UART1 */
+	USART1->CR1 |= 0x00002000;
+
+	/* enable GPIOA */
+	RCC->APB2ENR |= 0x00000004;
+	/* switch pa9/pa10 to uart1 */
+	GPIOA->CRH &= 0xfffff00f;
+	GPIOA->CRH |= 0x000008b0;
+}
+
+void initLED(void)
+{
+	/* enable GPIOA */
+	RCC->APB2ENR |= 0x00000004;
+	/* set PA5 to output push-pull mode */
+	GPIOA->CRL &= 0xff0fffff;
+	GPIOA->CRL |= 0x00300000;
+	/* set PA5 to High */
+	GPIOA->ODR |= 0x00000020;
+}
+
+void toggleLED(char cmd)
+{
+	if (cmd)
+		GPIOA->ODR |= 0x00000020;
+	else
+		GPIOA->ODR &= 0xffffffdf;
+}
+
+void initSysTick(void)
+{
+	/* select clock source to AHB/8 */
+	SysTick->CTRL |= 0x00000004;
+
+	/* enable assert SysTick exception request if count down to zero */
+	SysTick->CTRL |= 0x00000002;
+
+	/* set RELOAD value to 8000
+	 * clock source 64Mhz/ 8 = 8Mhz
+	 * 8M / 8000 = 1Khz => 1ms 
+	 */
+	SysTick->LOAD = 0x00001f40;
+
+	/* enable the counter */
+	SysTick->CTRL |= 0x00000001;
+}
+
+void SysTick_Handler(void)
+{
+	uwTick++;
+}
+
 int main(void)
 {
+	char cmd;
+	uint32_t tick;
+	initSysTick();
+	initUART();
+	initLED();
+
 	print_str("Hello World\r\n");
+	tick = uwTick;
+	cmd = 0;
+	while (1) {
+		if (uwTick - tick > 1000) {
+			tick = uwTick;
+			toggleLED(cmd);
+			cmd ^= 1;
+		}
+	}
 
 	return 1;
 }
--- reg.h
+++ reg.h
@@ -1,4 +1,7 @@
 #define uint32_t	unsigned int
+#define __IO		volatile
+#define __I		volatile const
+
 /*< Peripheral base address in the alias region */
 #define PERIPH_BASE           ((uint32_t)0x40000000) 
 
@@ -12,53 +15,74 @@
 #define GPIOA_BASE            (APB2PERIPH_BASE + 0x0800) /* 0x40010800 */
 #define GPIOB_BASE            (APB2PERIPH_BASE + 0x0C00)
 #define GPIOC_BASE            (APB2PERIPH_BASE + 0x1000)
-#define GPIOD_BASE            (APB2PERIPH_BASE + 0x1400)
+#define GPIOD_BASE            (APB2PERIPH_BASE + 0x1400) /* 0x40011400 */
 #define GPIOE_BASE            (APB2PERIPH_BASE + 0x1800)
+#define AFIO_BASE             (APB2PERIPH_BASE + 0x0000) /* 0x40010000 */
+
+/*< System Control Space Base Address  */
+#define SCS_BASE            (0xE000E000UL)                           
+/*< SysTick Base Address               */
+#define SysTick_BASE        (SCS_BASE +  0x0010UL)                    
 
 struct RCC_STR {
-  volatile uint32_t CR;			/* 0x40021000 */
-  volatile uint32_t CFGR;		/* 0x40021004 */
-  volatile uint32_t CIR;
-  volatile uint32_t APB2RSTR;
-  volatile uint32_t APB1RSTR;
-  volatile uint32_t AHBENR;
-  volatile uint32_t APB2ENR;
-  volatile uint32_t APB1ENR;
-  volatile uint32_t BDCR;
-  volatile uint32_t CSR;
+  __IO uint32_t CR;			/* 0x40021000 */
+  __IO uint32_t CFGR;			/* 0x40021004 */
+  __IO uint32_t CIR;			/* 0x40021008 */
+  __IO uint32_t APB2RSTR;		/* 0x4002100c */
+  __IO uint32_t APB1RSTR;		/* 0x40021010 */
+  __IO uint32_t AHBENR;                 /* 0x40021014 */
+  __IO uint32_t APB2ENR;                /* 0x40021018 */
+  __IO uint32_t APB1ENR;                /* 0x4002101c */
+  __IO uint32_t BDCR;
+  __IO uint32_t CSR;
 };
 
 struct FLASH_STR {
-  volatile uint32_t ACR;
-  volatile uint32_t KEYR;
-  volatile uint32_t OPTKEYR;
-  volatile uint32_t SR;
-  volatile uint32_t CR;
-  volatile uint32_t AR;
-  volatile uint32_t RESERVED;
-  volatile uint32_t OBR;
-  volatile uint32_t WRPR;
+  __IO uint32_t ACR;
+  __IO uint32_t KEYR;
+  __IO uint32_t OPTKEYR;
+  __IO uint32_t SR;
+  __IO uint32_t CR;
+  __IO uint32_t AR;
+  __IO uint32_t RESERVED;
+  __IO uint32_t OBR;
+  __IO uint32_t WRPR;
 };
 
 struct USART_STR {
-  volatile uint32_t SR;         /* Status register,   0x40013800      */
-  volatile uint32_t DR;         /* Data register,     0x40013804      */
-  volatile uint32_t BRR;        /* Baud rate register,0x40013808      */
-  volatile uint32_t CR1;        /* Control register 1,0x4001380c      */
-  volatile uint32_t CR2;        /* Control register 2,0x40013810      */
-  volatile uint32_t CR3;        /* Control register 3,0x40013814      */
-  volatile uint32_t GTPR;       /* Guard time and prescaler register, */
+  __IO uint32_t SR;         /* Status register,   0x40013800      */
+  __IO uint32_t DR;         /* Data register,     0x40013804      */
+  __IO uint32_t BRR;        /* Baud rate register,0x40013808      */
+  __IO uint32_t CR1;        /* Control register 1,0x4001380c      */
+  __IO uint32_t CR2;        /* Control register 2,0x40013810      */
+  __IO uint32_t CR3;        /* Control register 3,0x40013814      */
+  __IO uint32_t GTPR;       /* Guard time and prescaler register, */
 };
 
 struct GPIO_STR
 {
-  volatile uint32_t CRL;
-  volatile uint32_t CRH;
-  volatile uint32_t IDR;
-  volatile uint32_t ODR;
-  volatile uint32_t BSRR;
-  volatile uint32_t BRR;
-  volatile uint32_t LCKR;
+  __IO uint32_t CRL;
+  __IO uint32_t CRH;
+  __IO uint32_t IDR;
+  __IO uint32_t ODR;
+  __IO uint32_t BSRR;
+  __IO uint32_t BRR;
+  __IO uint32_t LCKR;
+};
+
+struct AFIO_STR {
+  __IO uint32_t EVCR;
+  __IO uint32_t MAPR;
+  __IO uint32_t EXTICR[4];
+  uint32_t RESERVED0;
+  __IO uint32_t MAPR2;  
+};
+
+struct SysTick_STR {
+  __IO uint32_t CTRL;                    /*!< Offset: 0x000 (R/W)  SysTick Control and Status Register */
+  __IO uint32_t LOAD;                    /*!< Offset: 0x004 (R/W)  SysTick Reload Value Register       */
+  __IO uint32_t VAL;                     /*!< Offset: 0x008 (R/W)  SysTick Current Value Register      */
+  __I  uint32_t CALIB;                   /*!< Offset: 0x00C (R/ )  SysTick Calibration Register        */
 };
 
 
@@ -70,3 +94,5 @@ struct GPIO_STR
 #define GPIOC               ((struct GPIO_STR *) GPIOC_BASE)
 #define GPIOD               ((struct GPIO_STR *) GPIOD_BASE)
 #define GPIOE               ((struct GPIO_STR *) GPIOE_BASE)
+#define AFIO                ((struct AFIO_STR *) AFIO_BASE)
+#define SysTick             ((struct SysTick_STR *) SysTick_BASE)   /*!< SysTick configuration struct       */
